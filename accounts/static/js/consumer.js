824f11d2704c1ec4627f18c0045c53bfb9369f10
mnnlthmpsn <mnnlthmpsn@outlook.com>
2020-07-23 22:18:00 +0000

    route drawn

--- accounts/static/js/consumer.js
+++ accounts/static/js/consumer.js
@@ -1,60 +1,73 @@
 var gmap
-var pharma
+var userLoc
+var pharmaLoc
+var directS
+var directD
+
 
 function initMap () {
-
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-
   var root = document.getElementById('map')
+  var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay = new google.maps.DirectionsRenderer();
 
-  calculateAndDisplayRoute = (directionsService, directionsDisplay) => {
-    directionsService.route({
-      origin: marker,
-      destination: pharma,
-      travelMode: 'WALKING',
-    }, (response, status) => {
-      status === 'OK' 
-        ? directionsDisplay.setDirections(response)
-        : window.alert(`Directions request failed due to ${status}`)
-    })
-  }
-
+  setdirectionService(directionsService)
+  setdirectionDisplay(directionsDisplay)
   // get user's position
   navigator.geolocation.getCurrentPosition(pos => {
-    lng = -pos.coords.longitude
+    lng = pos.coords.longitude
     lat = pos.coords.latitude
 
     var map = new google.maps.Map(root, { zoom: 13, center: { lat, lng } })
-    directionsDisplay.setMap(map)
     getMap(map)
-    // user locaiton
-    var marker = new google.maps.Marker({
-      position: { lat, lng },
-      map,
-      icon:
-        'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-    })
+    var user = new google.maps.LatLng(parseFloat(lat), parseFloat(lng))
+    setUser(user)
   })
 }
 
+
 // get pharmacy location
 function getpharmloc(lat, lng){
-  console.log(lat)
-  console.log(lng)
   let glat, glng
   glat = parseFloat(lat)
-  glng = -parseFloat(lng)
+  glng = parseFloat(lng)
 
   var myLatlng = new google.maps.LatLng(glat, glng);
-
-  var pharmloc = new google.maps.Marker({
-    position: myLatlng,
-    map: gmap
-  })
-  pharma = pharmloc
+  setPharma(myLatlng)
+  calcDistance(directS, directD)
 }
 
 function getMap(map) {
   gmap = map
+}
+
+function setUser(user){
+  userLoc = user
+}
+
+function setPharma(pharma){
+  pharmaLoc = pharma
+}
+
+function setdirectionDisplay(directionsDisplay) {
+  directD = directionsDisplay
+}
+
+function setdirectionService(directionsService) {
+  directS = directionsService
+}
+
+const calcDistance = (directS, directD) => {
+  directD.setMap(gmap)
+  directS.route({
+    origin: userLoc, 
+    destination: pharmaLoc,
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function(response, status) {
+    if (status === 'OK'){
+      directD.setDirections(response);
+    }
+    else {
+      window.alert(`Directions request failed due to ${status}`)
+    }
+  })
 }
