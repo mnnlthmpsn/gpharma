c04dc8a21f96fc9b4a37a434cc2f331767e3dc86
mnnlthmpsn <mnnlthmpsn@outlook.com>
2020-07-23 11:20:51 +0000

    useless

--- accounts/static/js/consumer.js
+++ accounts/static/js/consumer.js
@@ -1,7 +1,24 @@
 var gmap
+var pharma
 
 function initMap () {
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+
   var root = document.getElementById('map')
+
+  calculateAndDisplayRoute = (directionsService, directionsDisplay) => {
+    directionsService.route({
+      origin: marker,
+      destination: pharma,
+      travelMode: 'WALKING',
+    }, (response, status) => {
+      status === 'OK' 
+        ? directionsDisplay.setDirections(response)
+        : window.alert(`Directions request failed due to ${status}`)
+    })
+  }
 
   // get user's position
   navigator.geolocation.getCurrentPosition(pos => {
@@ -9,6 +26,7 @@ function initMap () {
     lat = pos.coords.latitude
 
     var map = new google.maps.Map(root, { zoom: 13, center: { lat, lng } })
+    directionsDisplay.setMap(map)
     getMap(map)
     // user locaiton
     var marker = new google.maps.Marker({
@@ -22,6 +40,8 @@ function initMap () {
 
 // get pharmacy location
 function getpharmloc(lat, lng){
+  console.log(lat)
+  console.log(lng)
   let glat, glng
   glat = parseFloat(lat)
   glng = -parseFloat(lng)
@@ -32,6 +52,7 @@ function getpharmloc(lat, lng){
     position: myLatlng,
     map: gmap
   })
+  pharma = pharmloc
 }
 
 function getMap(map) {
